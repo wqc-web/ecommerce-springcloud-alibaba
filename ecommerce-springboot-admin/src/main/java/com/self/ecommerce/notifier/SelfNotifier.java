a62e7f847b6862b0538fec2aa185c63d81b5c770
SelfRidicule <selfridicule@gmail.com>
2022-05-23 19:35:56 +0800

    描述：springbootadmin自定义告警

---
+++ ecommerce-springboot-admin/src/main/java/com/self/ecommerce/notifier/SelfNotifier.java
@@ -0,0 +1,35 @@
+package com.self.ecommerce.notifier;
+
+import de.codecentric.boot.admin.server.domain.entities.Instance;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
+import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
+import de.codecentric.boot.admin.server.notify.AbstractEventNotifier;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author wangqichao
+ */
+@Slf4j
+@Component
+public class SelfNotifier extends AbstractEventNotifier {
+    protected SelfNotifier(InstanceRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    protected Mono<Void> doNotify(InstanceEvent event, Instance instance) {
+        return Mono.fromRunnable(()->{
+            if(event instanceof InstanceStatusChangedEvent){
+                log.info("instance status change: [{}], [{}] , [{}]",
+                        instance.getRegistration().getName(),event.getInstance(),
+                        ((InstanceStatusChangedEvent)event).getStatusInfo().getStatus());
+            }else{
+                log.info("instance info: [{}] , [{}] , [{}]",
+                        instance.getRegistration().getName(),event.getInstance(),event.getType());
+            }
+        });
+    }
+}
